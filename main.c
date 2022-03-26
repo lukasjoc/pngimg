f9fd288b891932c0d043d51e341b2976c5a64f80
lukasjoc <jochamlu@gmail.com>
2022-03-26 17:27:25 +0100

    fix parsing all chunks

--- main.c
+++ main.c
@@ -12,82 +12,43 @@
 
 #define PNG_SIGNATURE_LENGTH 8
 #define BYTE_BOUNDARY 4
+#define PNG_LENGTH_MAX 32
 
-// Converts a byte array to a unsigned 32 bit int in (BE)
+// Converts a byte array to a unsigned 32 bit int in
 #define bytes_to_uint32_t(bytes) ((uint32_t)bytes[0] << 24) + ((uint32_t)bytes[1] << 16)  \
     + ((uint32_t)bytes[2] << 8) + (uint32_t)bytes[3]
 
 // puts n bytes from file by offset into bytes
-void read_n_bytes(FILE *file, uint32_t *bytes, size_t n, uint32_t offset) {
+void read_n_bytes(FILE *file, uint8_t *bytes, size_t n, uint32_t offset) {
     uint32_t l = 0;
+    printf("Reading Bytes offset from=%u to=%lu \n", offset, offset + n);
     fseek(file, offset, SEEK_SET);
-    char buffer;
-    while((buffer = fgetc(file)) != EOF && l < n) {
-        printf("Bytes in order: byte= %u \n", (uint8_t)buffer);
-        bytes[l++] = (uint8_t)buffer;
+    uint8_t byte;
+    while(l < n) {
+        byte = (uint8_t)fgetc(file);
+        if((int8_t)byte == EOF) break;
+        printf("Bytes in order: byte= %u, l=%u, n=%zu, \n", byte, l, n);
+        bytes[l++] = byte;
     }
 }
-
-typedef struct Chunk {
-    uint32_t *data;
-    uint32_t length[BYTE_BOUNDARY];
-    uint32_t type[BYTE_BOUNDARY];
-    uint32_t crc[BYTE_BOUNDARY];
-}Chunk;
 
 // the file is a valid PNG File
 bool check_png_signature(FILE* file, const char *png_file_path ) {
     const uint8_t PNG_SIGNATURE[PNG_SIGNATURE_LENGTH] = {
         137, 80, 78, 71, 13, 10, 26, 10
     };
-
     char buffer;
     uint8_t signature_length = 0;
     while((buffer = fgetc(file)) != EOF && signature_length < PNG_SIGNATURE_LENGTH) {
         uint8_t current_signature = PNG_SIGNATURE[signature_length];
         printf("Parsing Signature: path=%s, signature_length=%u, buffer=%u, png_buffer=%u\n",
-                png_file_path, signature_length, (uint8_t)buffer, current_signature);
-
+               png_file_path, signature_length, (uint8_t)buffer, current_signature);
         if((uint8_t)buffer!=current_signature) return false;
         signature_length++;
     }
     return true;
 }
 
-//uint32_t *read_length(FILE* file, uint32_t *offset) {
-//    uint32_t length[BYTE_BOUNDARY] = {0};
-//    puts("Parsing length");
-//    uint32_t *l = read_n_bytes(file, length, BYTE_BOUNDARY, *offset);
-//
-//    *offset += BYTE_BOUNDARY;
-//
-//}
-
-//uint32_t *read_type(FILE* file, uint32_t *offset) {
-//    uint32_t type[BYTE_BOUNDARY] = {0};
-//    puts("Parsing type");
-//    uint32_t *l = read_n_bytes(file, type, BYTE_BOUNDARY, *offset);
-//    *offset += BYTE_BOUNDARY;
-//    return l;
-//}
-//
-//uint32_t *read_data(FILE* file, uint32_t *offset, uint32_t length) {
-//    uint32_t *data = calloc(length, sizeof(uint32_t));
-//    printf("Parsing data: length=%u\n", length);
-//    uint32_t *l = read_n_bytes(file, data, length, *offset);
-//    *offset += length;
-//
-//    return l;
-//
-//}
-//
-//uint32_t *read_crc(FILE* file, uint32_t *offset) {
-//    uint32_t crc[BYTE_BOUNDARY] = {0};
-//    puts("Parsing crc");
-//    uint32_t *l = read_n_bytes(file, crc, BYTE_BOUNDARY, offset);
-//    *offset += BYTE_BOUNDARY;
-//    return l;
-//}
 
 // read all chunks
 void read_chunks(FILE *file) {
@@ -96,38 +57,34 @@ void read_chunks(FILE *file) {
 
     while((buffer = fgetc(file) != EOF)) {
         // Parse the length
-        uint32_t length[BYTE_BOUNDARY] = {0};
+        uint8_t length[BYTE_BOUNDARY] = {0};
         puts("Length START");
         read_n_bytes(file, length, BYTE_BOUNDARY, offset);
         offset += BYTE_BOUNDARY;
         puts("Length END");
-        // if(*length==NULL) break;
 
         // Parse the type
         puts("Type START");
-        uint32_t type[BYTE_BOUNDARY] = {0};
+        uint8_t type[BYTE_BOUNDARY] = {0};
         read_n_bytes(file, type, BYTE_BOUNDARY, offset);
         offset += BYTE_BOUNDARY;
-        puts("Type END");
-        // if(*type==NULL) break;
+        printf("Type END= %s\n", (char*)type);
 
-        // #if 1 for(unsigned int i = 0; i < 4; ++i) printf("length bytes == %d==%d \n", i, length[i]); #endif
-
+        // Try to parse the data
         puts("Data START");
         uint32_t length_into = bytes_to_uint32_t(length);
         printf("Data length uint32_t=%u\n", length_into);
-        uint32_t *data = (uint32_t* )calloc(length_into, sizeof(uint32_t));
+        uint8_t data[PNG_LENGTH_MAX*1024] = {0};
         read_n_bytes(file, data, length_into, offset);
         offset += length_into;
         puts("Data END");
 
         // Parse the crc
         puts("Crc START");
-        uint32_t crc[BYTE_BOUNDARY] = {0};
+        uint8_t crc[BYTE_BOUNDARY] = {0};
         read_n_bytes(file, crc, BYTE_BOUNDARY, offset);
         offset += BYTE_BOUNDARY;
         puts("Crc END");
-        // if(*crc==NULL) break;
     }
 }
 
